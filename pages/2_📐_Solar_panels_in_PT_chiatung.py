f2c1cd97e0a85b9408e6b112d98afadd0ae4349c
Magi0902 <magi23570902@gmail.com>
2024-11-29 20:13:18 +0800

    Update 2_📐_Solar_panels_in_PT_chiatung.py

--- pages/2_📐_Solar_panels_in_PT_chiatung.py
+++ pages/2_📐_Solar_panels_in_PT_chiatung.py
@@ -17,7 +17,7 @@ st.title("Solar Panels in Pingtung Map")
 
 with st.expander("See source code"):
     with st.echo():
-        m = leafmap.Map(center=(22.439, 120.538), zoom=4, height="600px") 
+        m = leafmap.Map(center=(22.439, 120.538), zoom=14, height="600px") 
         m.split_map(
             left_layer="HYBRID", right_layer="ESA WorldCover 2020"
         )
