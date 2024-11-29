42f0becf8a7845ae139051a2307305975d518ae0
Magi0902 <magi23570902@gmail.com>
2024-11-29 20:18:22 +0800

    Update 2_☀️_Solar_panels_in_PT_Chiatung.py

--- pages/2_☀️_Solar_panels_in_PT_Chiatung.py
+++ pages/2_☀️_Solar_panels_in_PT_Chiatung.py
@@ -19,8 +19,9 @@ with st.expander("See source code"):
     with st.echo():
         m = leafmap.Map(center=(22.439, 120.538), zoom=14, height="600px") 
         m.split_map(
-            left_layer="HYBRID", right_layer="ESA WorldCover 2020"
+            left_layer="HYBRID", right_layer="http://wmts.nlsc.gov.tw/wmts"
         )
         m.add_legend(title="ESA Land Cover", builtin_legend="ESA_WorldCover")
-
+#http://wmts.nlsc.gov.tw/wmts
+#ESA WorldCover 2020
 m.to_streamlit(height=700)
