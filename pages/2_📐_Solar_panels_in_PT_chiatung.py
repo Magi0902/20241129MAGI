f0053e378861ea21dc85659df6b7484a1849bec9
Magi0902 <magi23570902@gmail.com>
2024-11-29 20:10:45 +0800

    Update 2_📐_Solar_panels_in_PT_chiatung.py

--- pages/2_📐_Solar_panels_in_PT_chiatung.py
+++ pages/2_📐_Solar_panels_in_PT_chiatung.py
@@ -13,13 +13,13 @@ st.sidebar.info(markdown)
 logo = "https://i.imgur.com/UbOXYAU.png"
 st.sidebar.image(logo)
 
-st.title("Solar_Panels_in_Pingtung Map")
+st.title("Solar Panels in Pingtung Map")
 
 with st.expander("See source code"):
     with st.echo():
         m = leafmap.Map()
         m.split_map(
-            left_layer="ESA WorldCover 2020 S2 FCC", right_layer="ESA WorldCover 2020"
+            left_layer="HYBRID", right_layer="ESA WorldCover 2020"
         )
         m.add_legend(title="ESA Land Cover", builtin_legend="ESA_WorldCover")
 
