c5b6fac922161c2f33ffb673e35e21d9df37c443
Magi0902 <magi23570902@gmail.com>
2024-12-01 11:12:30 +0800

    Update 2_☀️_Solar_panels_in_PT_Chiatung.py

--- pages/2_☀️_Solar_panels_in_PT_Chiatung.py
+++ pages/2_☀️_Solar_panels_in_PT_Chiatung.py
@@ -19,9 +19,9 @@ with st.expander("See source code"):
     with st.echo():
         m = leafmap.Map(center=(22.439, 120.538), zoom=14, height="600px") 
         m.split_map(
-            left_layer="HYBRID", right_layer="https://chinchillaz.github.io/streamlit-hw/4towns_solarpanels.geojson"
+            left_layer="HYBRID", right_layer="ESA WorldCover 2020"
         )
-       # m.add_legend(title="ESA Land Cover", builtin_legend="ESA_WorldCover")
+        m.add_legend(title="ESA Land Cover", builtin_legend="ESA_WorldCover")
 #WORLDCOVER_2020_S2_FCC
 #ESA WorldCover 2020
 #https://chinchillaz.github.io/streamlit-hw/4towns_solarpanels.geojson
